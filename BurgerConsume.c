3e307a4669714ffb20c941b0e4aaa5dfc23f8f7b
Angelo Groß <angelo.gross@education-siemens.com>
2014-02-04 20:38:14 +0100

    initial commit

---
+++ BurgerConsume.c
@@ -0,0 +1,24 @@
+/* This is the original master file.
+ * It shall consist of two functions as given below
+ * and the main function.
+ * The FirstSim programmer must program the "addup()" 
+ * function while the SecondSim programmer must
+ * program the "divide()" function.*/
+
+#include <stdio.h>
+
+/*This function shall add up the total Burgers of the three
+ * programmer Team member consumed during the last week and
+ * return this value.*/
+addup()
+
+/*This function shall calculate the last week average consume
+ * of the programmer*/
+divide()
+
+int main(void)
+{
+	int iTotalBurgers = addup(3,7,12);
+	printf("The average of Burgers consumed by each team member was %.2f",divide(iTotalBurgers,3));
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# This is a branching example as given in the third
+# git/github tutorial. FirstSim will simulate one PC
+# while SecondSim shall simulate an other.
+# Each of them therefore is a repository.
